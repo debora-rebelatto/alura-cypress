f13e2c3131a64c88b9e1c86abf57f3136178432b
debora-rebelatto <deborarebelatto1@gmail.com>
2023-05-04 01:35:09 -0300

    commands

--- cypress/e2e/inicio.cy.js
+++ cypress/e2e/inicio.cy.js
@@ -1,6 +1,9 @@
-describe('template spec', () => {
-  it('Deve renderizar corretamente o texto da seção de vantagens ', () => {
+describe('Página Inicial', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:3000/');
-    cy.get('h2').contains('Vantagens do nosso banco:');
+  });
+
+  it('Deve renderizar corretamente o texto da seção de vantagens ', () => {
+    cy.getByData('titulo-principal').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!');
   });
 })
